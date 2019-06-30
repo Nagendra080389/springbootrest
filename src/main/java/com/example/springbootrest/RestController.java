97897c417b0179adec2b22b96d336f3370e9c3e3
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-06-30 13:25:45 +0530

    i

--- src/main/java/com/example/springbootrest/RestController.java
+++ src/main/java/com/example/springbootrest/RestController.java
@@ -1,7 +1,10 @@
 package com.example.springbootrest;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +14,12 @@ import java.io.IOException;
 public class RestController {
 
     @RequestMapping(value = "/consumeFiles", method = RequestMethod.POST)
-    public String consumeRest(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public String consumeRest(@RequestParam("filename") MultipartFile filename) throws IOException {
 
-        System.out.println(request);
-        System.out.println(response);
+        System.out.println(filename.getName());
+        System.out.println(filename.getOriginalFilename());
+        System.out.println(filename.getSize());
+
         return "";
 
     }
