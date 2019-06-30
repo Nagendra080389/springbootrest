dd1396e8924c614a7f43987e03ee2abfb929cc1d
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-06-30 21:38:17 +0530

    i

---
+++ src/main/java/com/example/springbootrest/FileAttachmentMerged.java
@@ -0,0 +1,40 @@
+package com.example.springbootrest;
+
+public class FileAttachmentMerged {
+    private String strName;
+    private String strUploadedDate;
+    private String strUploadedBy;
+    private String strContentType;
+
+    public String getStrName() {
+        return strName;
+    }
+
+    public void setStrName(String strName) {
+        this.strName = strName;
+    }
+
+    public String getStrUploadedDate() {
+        return strUploadedDate;
+    }
+
+    public void setStrUploadedDate(String strUploadedDate) {
+        this.strUploadedDate = strUploadedDate;
+    }
+
+    public String getStrUploadedBy() {
+        return strUploadedBy;
+    }
+
+    public void setStrUploadedBy(String strUploadedBy) {
+        this.strUploadedBy = strUploadedBy;
+    }
+
+    public String getStrContentType() {
+        return strContentType;
+    }
+
+    public void setStrContentType(String strContentType) {
+        this.strContentType = strContentType;
+    }
+}
--- src/main/java/com/example/springbootrest/RestController.java
+++ src/main/java/com/example/springbootrest/RestController.java
@@ -1,5 +1,6 @@
 package com.example.springbootrest;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,20 +10,39 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
     @CrossOrigin(origins = "https://nagesingh-dev-ed--c.visualforce.com")
     @RequestMapping(value = "/consumeFiles", method = RequestMethod.POST)
-    public String consumeRest(@RequestParam("filename")MultipartFile filename) throws IOException {
+    public List<FileAttachmentMerged> consumeRest(@RequestParam("filename")MultipartFile filename[]) throws IOException {
+        //create a temp file
+        File temp = File.createTempFile("temp-file-name", ".tmp");
 
-        System.out.println(filename.getName());
-        System.out.println(filename.getOriginalFilename());
-        System.out.println(filename.getSize());
+        System.out.println("Temp file : " + temp.getAbsolutePath());
 
-        return "";
+        //Get tempropary file path
+        String absolutePath = temp.getAbsolutePath();
+        List<FileAttachmentMerged> fileAttachmentMergeds = new ArrayList<>();
+        for (MultipartFile uploadedFile : filename) {
+            File file = new File( absolutePath + uploadedFile.getOriginalFilename());
+            uploadedFile.transferTo(file);
+            FileAttachmentMerged fileAttachmentMerged = new FileAttachmentMerged();
+            fileAttachmentMerged.setStrName(uploadedFile.getName());
+            fileAttachmentMerged.setStrContentType("Type");
+            fileAttachmentMerged.setStrUploadedBy("Nagendra Singh");
+            fileAttachmentMerged.setStrUploadedDate(new Date().toString());
+            fileAttachmentMergeds.add(fileAttachmentMerged);
+        }
+
+
+        return fileAttachmentMergeds;
 
     }
 }
