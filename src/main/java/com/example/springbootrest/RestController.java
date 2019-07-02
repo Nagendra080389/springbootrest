99d3f525cb4b46b6d8498fffbf2693aaea86a62c
Nagendra Kumar Singh <nagendra080389@gmail.com>
2019-07-02 16:28:20 +0530

    Update RestController.java

--- src/main/java/com/example/springbootrest/RestController.java
+++ src/main/java/com/example/springbootrest/RestController.java
@@ -20,12 +20,32 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    @CrossOrigin(origins = "https://nagesingh-dev-ed--c.visualforce.com")
+    @CrossOrigin(origins = {"https://nagesingh-dev-ed--c.visualforce.com", "chrome-extension://aejoelaoggembcahagimdiliamlcdmfm"})
     @RequestMapping(value = "/consumeFiles", method = RequestMethod.POST)
-    public String consumeRest(@RequestParam("filename")MultipartFile filename[]) throws IOException {
-        
+    public String consumeRest(@RequestParam("filename")MultipartFile filename[]) throws IOException {	    public String consumeRest(@RequestParam("filename")MultipartFile filename[]) throws IOException {
+        //create a temp file	
+         File temp = File.createTempFile("temp-file-name", ".tmp");	
+ObjectMapper mapper = new ObjectMapper();	
+        System.out.println("Temp file : " + temp.getAbsolutePath());	
 
-        return "";
 
-    }
+         //Get tempropary file path	        return "";
+        String absolutePath = temp.getAbsolutePath();	
+        List<FileAttachmentMerged> fileAttachmentMergeds = new ArrayList<>();	
+        for (MultipartFile uploadedFile : filename) {	
+            File file = new File( absolutePath + uploadedFile.getOriginalFilename());	
+            uploadedFile.transferTo(file);	
+            FileAttachmentMerged fileAttachmentMerged = new FileAttachmentMerged();	
+            fileAttachmentMerged.setStrName(uploadedFile.getName());	
+            fileAttachmentMerged.setStrContentType("Type");	
+            fileAttachmentMerged.setStrUploadedBy("Nagendra Singh");	
+            fileAttachmentMerged.setStrUploadedDate(new Date().toString());	
+            fileAttachmentMergeds.add(fileAttachmentMerged);	
+        }	
+String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(fileAttachmentMergeds);	
+
+         return jsonInString2;	
+
+
+     }
 }
